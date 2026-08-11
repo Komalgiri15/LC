8927a9d8ec2aecaafce9afc32b613f2ea3e33c2c
Komalgiri15 <komal.giri@lmsathena.com>
2026-08-11 13:00:52 +0530

    Add padding to prevent circle quote from overlapping text

--- src/components/Spread1.tsx
+++ src/components/Spread1.tsx
@@ -82,7 +82,7 @@ export default function Spread1() {
           <path fill="#111111" d="M44.7,-76.4C58.9,-69.2,71.8,-59.1,81.4,-46.3C91,-33.5,97.3,-18,97.7,-2.3C98.1,13.4,92.5,29.3,82.8,42.5C73.1,55.7,59.3,66.2,44.4,73.1C29.5,80,13.5,83.3,-2.2,86.9C-17.9,90.5,-33.3,94.4,-47.1,89.5C-60.9,84.6,-73,70.9,-81.1,56C-89.2,41.1,-93.3,25,-94.1,9.4C-94.9,-6.2,-92.4,-21.3,-84.9,-33.6C-77.4,-45.9,-64.9,-55.4,-51.7,-63.1C-38.5,-70.8,-24.6,-76.7,-10,-79.1C4.6,-81.5,19.2,-80.4,30.5,-83.6L44.7,-76.4Z" transform="translate(100 100) scale(1.5)" />
         </svg>
 
-        <div style={{ marginTop: '60px', zIndex: 2, position: 'relative' }} className="text-center">
+        <div style={{ marginTop: '60px', zIndex: 2, position: 'relative', paddingBottom: '350px' }} className="text-center">
           <h2 className="title-dark font-heading">A NOTE</h2>
           <p className="subtitle-dark font-heading">FROM LUKE</p>
           
